d35b2cb43597583dbe4eef249f0db17ecf289dd8
LinconDash <lincondash01@gmail.com>
2025-03-04 17:44:32 +0530

    Added Task 6

--- Task- 6 Python Django/textutils/textutils/urls.py
+++ Task- 6 Python Django/textutils/textutils/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for textutils project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('clean', views.clean, name="clean")
 ]
